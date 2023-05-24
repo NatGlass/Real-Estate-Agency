8d369e2d78843eb11859b1d7f95c0874256157cc
Nathan G <Nathanglass26@gmail.com>
2023-05-24 19:37:04 +0100

    make the button styling global

--- components/ButtonLink.js
+++ components/ButtonLink.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const ButtonLink = ({ destination, label }) => {
     return (
-        <Link href={destination} className="bg-blue-700 hover:bg-blue-600 inline-block my-2 px-4 py-2 rounded-md cursor-pointer text-white">
+        <Link href={destination} className="btn">
             {label}
         </Link>
     )
